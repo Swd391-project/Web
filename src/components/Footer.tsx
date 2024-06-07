fa1c388cf970ca374eea1b60415e9e45bdd2cd82
TyKay264 <147971434+TyKay264@users.noreply.github.com>
2024-06-07 14:29:45 +0700

    setup front end (#2)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="copyright">
+                <p className="mb-0">
+                    Copyright © Designed &amp; Developed by{" "}
+                    <br />
+                    <a href="uxign.com" target="_blank">
+                        Better to fight
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;

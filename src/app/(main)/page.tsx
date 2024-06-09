896e166f57495006fafccf4abb16dc63c7295e0b
tnmd130103 <tnmd130103@gmail.com>
2024-06-09 19:42:45 +0700

    add component and template for dashboard

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import Navbar from "@/components/Navbar";
+import BoxList from "@/components/Dashboard/BoxList";
+import WaveChart from "@/components/Dashboard/WaveChart";
+import NewBirdList from "@/components/Dashboard/NewBirdList";
+import ColumnChart from "@/components/Dashboard/ColumnChart";
+import PieChart from "@/components/Dashboard/PieChart";
+
 import React, { useEffect } from "react";
 
 const page = () => {
@@ -7,6 +13,33 @@ const page = () => {
     <section className="home">
       <div className="home-content">
         <header className="home-header"></header>
+        <div className="content-body">
+          <div className="warper container-fluid">
+            <div className="col-sm-6 p-md-0">
+              <div className="welcome-text">
+                <h4 className="container">Dashboard</h4>
+              </div>
+            </div>
+            <div className="new-patients main_container">
+              <BoxList />
+
+              <div className="row">
+                <div className="col-lg-8">
+                  <WaveChart />
+                </div>
+                <div className="col-lg-4">
+                  <NewBirdList />
+                </div>
+                <div className="col-lg-6">
+                  <PieChart />
+                </div>
+                <div className="col-lg-6">
+                  <ColumnChart />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Dashboard/PieChart.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const PieChart = () => {
+  return (
+    <div className="card shadow widget1">
+      <div className="card-header">
+        <h4 className="card-title">Thống kê giai đoạn</h4>
+      </div>
+      <div className="card-body">
+        <div className="row justify-content-center">
+          <div className="col-lg-12">
+            <canvas id="chart3" width="100%" height={220} />
+          </div>
+          <div className="col-lg-10">
+            <ul className="d-flex justify-content-between m-t-30">
+              <li className="content-widget text-center">
+                <p className="mb-0 fs-14 text-muted">Số lượng A</p>
+                <h4 className="mb-0 fs-20 text-dark-gray">639</h4>
+              </li>
+              <li className="content-widget text-center">
+                <p className="mb-0 fs-14 text-muted">Số lượng B</p>
+                <h4 className="mb-0 fs-20 text-dark-gray">285</h4>
+              </li>
+              <li className="content-widget text-center">
+                <p className="mb-0 fs-14 text-muted">Số lượng C</p>
+                <h4 className="mb-0 fs-20 text-dark-gray">147</h4>
+              </li>
+              <li className="content-widget text-center">
+                <p className="mb-0 fs-14 text-muted">Số lượng D</p>
+                <h4 className="mb-0 fs-20 text-dark-gray">360</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
